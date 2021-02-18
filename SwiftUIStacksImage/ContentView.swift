987b401108982ce20ee3a7194a53efbffb547ba7
pooya <info@centurytrail.com>
2021-02-18 03:21:24 -0800

    Initial Commit

---
+++ SwiftUIStacksImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStacksImage
+//
+//  Created by Pooya on 2021-02-18.
+//  Copyright © 2021 Pooya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
